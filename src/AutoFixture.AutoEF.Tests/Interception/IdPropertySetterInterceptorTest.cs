2f6fd2f895f2393d52f9b97ecaad5603cef17914
Alex Gill <alex.gill@penna.com>
2014-01-30 18:15:33 +0000

    IdPropertyInterceptorTest

---
+++ src/AutoFixture.AutoEF.Tests/Interception/IdPropertySetterInterceptorTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture.AutoEF.Interception;
+using AutoFixture.AutoEF.Tests.MockEntities;
+using Castle.DynamicProxy;
+using FluentAssertions;
+using NSubstitute;
+using System;
+using Xunit.Extensions;
+
+namespace AutoFixture.AutoEF.Tests.Interception
+{
+    using SUT = IdPropertySetterInterceptor;
+
+    public class IdPropertySetterInterceptorTest
+    {
+        [Theory, AutoNSub]
+        public void SutIsInterceptor(SUT sut)
+        {
+            sut.Should().BeAssignableTo<IInterceptor>();
+        }
+
+        [Theory, AutoNSub]
+        public void ThrowsOnNullInvocation(SUT sut)
+        {
+            sut.Invoking(s => s.Intercept(null))
+                .ShouldThrow<ArgumentNullException>();
+        }
+
+        [Theory, AutoNSub]
+        public void SetsIdOnMatchingProperty(SUT sut, IInvocation invocation, int barId)
+        {
+            var foo = new Foo { BarId = barId };
+            var bar = new Bar();
+
+            invocation.Method.Returns(foo.GetType().GetProperty("Bar").GetGetMethod());
+            invocation.InvocationTarget.Returns(foo);
+            invocation.ReturnValue = bar;
+
+            sut.Intercept(invocation);
+
+            bar.Id.Should().Be(barId);
+        }
+    }
+}
