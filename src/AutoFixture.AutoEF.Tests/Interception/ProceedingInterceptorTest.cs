0ae088bb5d98e43bad4b4fd749630cec69e127f4
Alex Gill <alex.gill@penna.com>
2014-01-30 18:28:15 +0000

    ProceedingInterceptorTest

---
+++ src/AutoFixture.AutoEF.Tests/Interception/ProceedingInterceptorTest.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture.AutoEF.Interception;
+using Castle.DynamicProxy;
+using FluentAssertions;
+using NSubstitute;
+using Ploeh.AutoFixture.Xunit;
+using System;
+using Xunit.Extensions;
+
+namespace AutoFixture.AutoEF.Tests.Interception
+{
+    public class ProceedingInterceptorTest
+    {
+        [Theory, AutoData]
+        public void SutIsInterceptor(ProceedingInterceptor sut)
+        {
+            sut.Should().BeAssignableTo<IInterceptor>();
+        }
+
+        [Theory, AutoData]
+        public void ThrowsOnNullInvocation(ProceedingInterceptor sut)
+        {
+            sut.Invoking(s => s.Intercept(null))
+                .ShouldThrow<ArgumentNullException>();
+        }
+
+        [Theory, AutoNSub]
+        public void CallsProceedOnInvocation(ProceedingInterceptor sut, IInvocation invocation)
+        {
+            sut.Intercept(invocation);
+
+            invocation.Received().Proceed();
+        }
+    }
+}
