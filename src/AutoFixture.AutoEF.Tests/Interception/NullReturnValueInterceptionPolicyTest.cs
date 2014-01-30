ae9335e6347d6f3fab34ec7cb1d86ee6ab479f11
Alex Gill <alex.gill@penna.com>
2014-01-30 18:21:26 +0000

    NullInterceptorTest, NullReturnValueInterceptorPolicyTest

---
+++ src/AutoFixture.AutoEF.Tests/Interception/NullReturnValueInterceptionPolicyTest.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.AutoEF.Interception;
+using Castle.DynamicProxy;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit;
+using System;
+using Xunit.Extensions;
+
+namespace AutoFixture.AutoEF.Tests.Interception
+{
+    using SUT = NullReturnValueInterceptionPolicy;
+
+    public class NullReturnValueInterceptionPolicyTest
+    {
+        [Theory, AutoData]
+        public void SutIsInterceptionPolicy(SUT sut)
+        {
+            sut.Should().BeAssignableTo<IInterceptionPolicy>();
+        }
+
+        [Theory, AutoData]
+        public void ThrowsOnNullInvocation(SUT sut)
+        {
+            sut.Invoking(s => s.ShouldIntercept(null))
+                .ShouldThrow<ArgumentNullException>();
+        }
+
+        [Theory, AutoNSub]
+        public void NullReturnValueReturnsTrue(SUT sut, IInvocation invocation)
+        {
+            invocation.ReturnValue = null;
+
+            var result = sut.ShouldIntercept(invocation);
+
+            result.Should().BeTrue();
+        }
+
+        [Theory, AutoNSub]
+        public void NonNullReturnValueReturnsFalse(SUT sut, IInvocation invocation, object value)
+        {
+            invocation.ReturnValue = value;
+
+            var result = sut.ShouldIntercept(invocation);
+
+            result.Should().BeFalse();
+        }
+    }
+}
