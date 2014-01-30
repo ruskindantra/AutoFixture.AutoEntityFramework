1c7a59af11fc9df6f4569aa3ef105b3efdfd7f50
Alex Gill <alex.gill@penna.com>
2014-01-30 18:28:15 +0000

    ProceedingInterceptorTest

--- src/AutoFixture.AutoEF/Interception/ProceedingInterceptor.cs
+++ src/AutoFixture.AutoEF/Interception/ProceedingInterceptor.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.DynamicProxy;
+using System;
 
 namespace AutoFixture.AutoEF.Interception
 {
@@ -6,6 +7,9 @@ namespace AutoFixture.AutoEF.Interception
     {
         public void Intercept(IInvocation invocation)
         {
+            if (invocation == null)
+                throw new ArgumentNullException("invocation");
+
             invocation.Proceed();
         }
     }
