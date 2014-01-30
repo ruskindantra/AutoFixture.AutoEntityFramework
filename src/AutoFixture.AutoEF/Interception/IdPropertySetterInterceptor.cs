6f5fbe5177f55234eecb5bea4e3b425f65a5c0c4
Alex Gill <alex.gill@penna.com>
2014-01-30 18:15:33 +0000

    IdPropertyInterceptorTest

--- src/AutoFixture.AutoEF/Interception/IdPropertySetterInterceptor.cs
+++ src/AutoFixture.AutoEF/Interception/IdPropertySetterInterceptor.cs
@@ -1,15 +1,22 @@
 ﻿using Castle.DynamicProxy;
+using System;
 
 namespace AutoFixture.AutoEF.Interception
 {
+    /// <summary>
+    /// Sets the "Id" property of the invocation.ReturnValue to
+    /// the value of a matching target.___Id property if it exists
+    /// </summary>
     public class IdPropertySetterInterceptor : IInterceptor
     {
         public void Intercept(IInvocation invocation)
         {
+            if (invocation == null)
+                throw new ArgumentNullException("invocation");
+
             var propertyName = invocation.Method.Name.Substring(4);
             var target = invocation.InvocationTarget;
 
-            // If the target object has a matching xxxxId property, set it for consistency
             var idProp = target.GetType().GetProperty(propertyName + "Id");
             var proxyIdProp = invocation.ReturnValue.GetType().GetProperty("Id");
 
