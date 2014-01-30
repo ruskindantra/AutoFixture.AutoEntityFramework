11531e871a2f1a2608e25b97b751a191b7e21747
Alex Gill <alex.gill@penna.com>
2014-01-30 17:11:02 +0000

    AndInterceptionPolicyTest

---
+++ src/AutoFixture.AutoEF.Tests/AutoNSubAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoNSubstitute;
+using Ploeh.AutoFixture.Xunit;
+
+namespace AutoFixture.AutoEF.Tests
+{
+    public class AutoNSubAttribute : AutoDataAttribute
+    {
+        public AutoNSubAttribute()
+            : base(new Fixture().Customize(new AutoNSubstituteCustomization()))
+        { }
+    }
+
+    public class InlineAutoNSubAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoNSubAttribute(params object[] values)
+            : base(new AutoNSubAttribute(), values)
+        { }
+    }
+}
---
+++ src/AutoFixture.AutoEF.Tests/Interception/AndInterceptionPolicyTest.cs
@@ -0,0 +1,72 @@
+﻿using AutoFixture.AutoEF.Interception;
+using Castle.DynamicProxy;
+using FluentAssertions;
+using NSubstitute;
+using System;
+using Xunit;
+using Xunit.Extensions;
+
+namespace AutoFixture.AutoEF.Tests.Interception
+{
+    public class AndInterceptionPolicyTest
+    {
+        [Fact]
+        public void ThrowsOnNullPolicies()
+        {
+            Action act = () => new AndInterceptionPolicy(null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Theory, AutoNSub]
+        public void ThrowsOnNullInvocation(AndInterceptionPolicy sut)
+        {
+            sut.Invoking(s => s.ShouldIntercept(null))
+                .ShouldThrow<ArgumentNullException>();
+        }
+
+        [Theory, AutoNSub]
+        public void ReturnsTrueWhenNoPolicies(IInvocation invocation)
+        {
+            var sut = new AndInterceptionPolicy();
+
+            var result = sut.ShouldIntercept(invocation);
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineAutoNSub(true)]
+        [InlineAutoNSub(false)]
+        public void ReturnsValueOfSinglePolicy(bool expected, IInterceptionPolicy policy, IInvocation invocation)
+        {
+            policy.ShouldIntercept(invocation).Returns(expected);
+
+            var sut = new AndInterceptionPolicy(policy);
+
+            var result = sut.ShouldIntercept(invocation);
+
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineAutoNSub(false, false, false)]
+        [InlineAutoNSub(false, true, false)]
+        [InlineAutoNSub(true, false, false)]
+        [InlineAutoNSub(true, true, true)]
+        public void ReturnsAllOfMultiplePolicies(bool a, bool b, bool expected,
+            IInterceptionPolicy policyA,
+            IInterceptionPolicy policyB,
+            IInvocation invocation)
+        {
+            policyA.ShouldIntercept(invocation).Returns(a);
+            policyB.ShouldIntercept(invocation).Returns(b);
+
+            var sut = new AndInterceptionPolicy(policyA, policyB);
+
+            var result = sut.ShouldIntercept(invocation);
+
+            result.Should().Be(expected);
+        }
+    }
+}
