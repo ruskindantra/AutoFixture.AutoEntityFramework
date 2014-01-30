9b4dd2442b8baecabaf06d07aa994fb99f004413
Alex Gill <alex.gill@penna.com>
2014-01-30 12:22:34 +0000

    Cached the results from EntityTypesProvider in a set

---
+++ src/AutoFixture.AutoEF/CachedEntityTypesProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoFixture.AutoEF
+{
+    public class CachedEntityTypesProvider : IEntityTypesProvider
+    {
+        private readonly Lazy<HashSet<Type>> _types;
+
+        public CachedEntityTypesProvider(IEntityTypesProvider decorated)
+        {
+            _types = new Lazy<HashSet<Type>>(() => new HashSet<Type>(decorated.GetTypes()));
+        }
+
+        public IEnumerable<Type> GetTypes()
+        {
+            return _types.Value;
+        }
+    }
+}
