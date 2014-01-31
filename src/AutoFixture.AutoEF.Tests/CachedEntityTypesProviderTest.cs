d7587a490cd742c087f6a46e5089b479d15c2b10
Alex Gill <alex.gill@penna.com>
2014-01-31 13:43:20 +0000

    CachedEntityTypesProviderTest

---
+++ src/AutoFixture.AutoEF.Tests/CachedEntityTypesProviderTest.cs
@@ -0,0 +1,28 @@
+﻿using AutoFixture.AutoEF.Tests.MockEntities;
+using FluentAssertions;
+using NSubstitute;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+
+namespace AutoFixture.AutoEF.Tests
+{
+    public class CachedEntityTypesProviderTest
+    {
+        [Theory, AutoNSub]
+        public void SutIsEntityTypesProvider(CachedEntityTypesProvider sut)
+        {
+            sut.Should().BeAssignableTo<IEntityTypesProvider>();
+        }
+
+        [Theory, AutoNSub]
+        public void SutIsDecorator([Frozen] IEntityTypesProvider decorated, CachedEntityTypesProvider sut)
+        {
+            var types = new[] { typeof (Foo), typeof (Bar), typeof (Qux) };
+            decorated.GetTypes().Returns(types);
+
+            var result = sut.GetTypes();
+
+            result.Should().BeEquivalentTo(types);
+        }
+    }
+}
