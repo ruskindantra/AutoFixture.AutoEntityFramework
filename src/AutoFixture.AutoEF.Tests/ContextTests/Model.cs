81be22a5ab9d9b7921421820cd580f26a718f594
Alex Gill <alexandermichaelfoxgill@hotmail.co.uk>
2015-02-23 23:38:08 +0000

    sticking this in source control for sharing purposes

---
+++ src/AutoFixture.AutoEF.Tests/ContextTests/Model.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace AutoFixture.AutoEF.Tests.ContextTests
+{
+    class Foo
+    {
+        public int FooId { get; set; }
+
+        public int? BarId { get; set; }
+        public int UnconventionalBarId { get; set; }
+        public virtual Bar Bar { get; set; }
+
+        public virtual ICollection<Bar> Bars { get; set; }
+    }
+
+    class Bar
+    {
+        public int BarId { get; set; }
+
+        public int? FooId { get; set; }
+        public virtual Foo Foo { get; set; }
+
+        public virtual ICollection<Foo> Foos { get; set; }
+    }
+
+    class OptionalToRequiredContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.UnconventionalBarId)
+                .Ignore(f => f.Bars);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.FooId)
+                .Ignore(b => b.Foos);
+
+            builder.Entity<Foo>()
+                .HasOptional(f => f.Bar)
+                .WithRequired(b => b.Foo);
+        }
+    }
+
+    class OptionalToOptionalContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.UnconventionalBarId)
+                .Ignore(f => f.Bars);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.FooId)
+                .Ignore(b => b.Foos);
+
+            builder.Entity<Foo>()
+                .HasOptional(f => f.Bar)
+                .WithOptionalPrincipal(b => b.Foo);
+        }
+    }
+
+    class NoKeyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.BarId)
+                .Ignore(f => f.UnconventionalBarId)
+                .Ignore(f => f.Bars);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.FooId)
+                .Ignore(b => b.Foos);
+
+            builder.Entity<Foo>()
+                .HasOptional(f => f.Bar)
+                .WithOptionalPrincipal(b => b.Foo);
+        }
+    }
+
+    class UnidirectionalContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.BarId)
+                .Ignore(f => f.UnconventionalBarId)
+                .Ignore(f => f.Bars);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.FooId)
+                .Ignore(b => b.Foo)
+                .Ignore(b => b.Foos);
+
+            builder.Entity<Foo>()
+                .HasOptional(f => f.Bar);
+        }
+    }
+
+    class UnconventionalKeyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.BarId)
+                .Ignore(f => f.Bars);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.FooId)
+                .Ignore(b => b.Foos);
+
+            builder.Entity<Foo>()
+                .HasOptional(f => f.Bar)
+                .WithOptionalPrincipal(b => b.Foo)
+                .Map(f => f.MapKey("UnconventionalBarId"));
+        }
+    }
+
+    class OneToManyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.BarId)
+                .Ignore(f => f.Bar)
+                .Ignore(f => f.UnconventionalBarId);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.Foos);
+
+            builder.Entity<Foo>()
+                .HasMany(f => f.Bars)
+                .WithRequired(b => b.Foo)
+                .HasForeignKey(b => b.FooId);
+        }
+    }
+
+    class ManyToManyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Foo>()
+                .HasKey(f => f.FooId)
+                .Ignore(f => f.BarId)
+                .Ignore(f => f.Bar)
+                .Ignore(f => f.UnconventionalBarId);
+
+            builder.Entity<Bar>()
+                .HasKey(b => b.BarId)
+                .Ignore(b => b.FooId)
+                .Ignore(b => b.Foo);
+
+            builder.Entity<Foo>()
+                .HasMany(f => f.Bars)
+                .WithMany(b => b.Foos);
+        }
+    }
+}
---
+++ src/AutoFixture.AutoEF.Tests/DbModelExploration.cs
@@ -0,0 +1,96 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using AutoFixture.AutoEF.Tests.ContextTests;
+using Xunit;
+
+namespace AutoFixture.AutoEF.Tests
+{
+    public class DbModelExploration
+    {
+        private DbContext Get<TContext>() where TContext : DbContext, new()
+        {
+            Database.SetInitializer<TContext>(null);
+            return new TContext();
+        }
+
+        [Fact]
+        public void TestOptionalToRequired()
+        {
+            using (var context = Get<OptionalToRequiredContext>())
+                GetAllForeignKeys(context);
+        }
+
+        [Fact]
+        public void TestConfigureOptionalToOptional()
+        {
+            using (var context = Get<OptionalToOptionalContext>())
+                GetAllForeignKeys(context);
+        }
+
+        [Fact]
+        public void TestNoKey()
+        {
+            using (var context = Get<NoKeyContext>())
+                GetAllForeignKeys(context);
+        }
+
+        [Fact]
+        public void TestUnidirectional()
+        {
+            using (var context = Get<UnidirectionalContext>())
+                GetAllForeignKeys(context);
+        }
+
+        [Fact]
+        public void TestUnconventionalKey()
+        {
+            using (var context = Get<UnconventionalKeyContext>())
+                GetAllForeignKeys(context);
+        }
+
+        [Fact]
+        public void TestOneToMany()
+        {
+            using (var context = Get<OneToManyContext>())
+                GetAllForeignKeys(context);
+        }
+
+        [Fact]
+        public void TestManyToMany()
+        {
+            using (var context = Get<ManyToManyContext>())
+                GetAllForeignKeys(context);
+        }
+
+        public void GetAllForeignKeys(DbContext context)
+        {
+            var metadata = ((IObjectContextAdapter) context).ObjectContext.MetadataWorkspace;
+
+            var oCollection = (ObjectItemCollection)metadata.GetItemCollection(DataSpace.OSpace);
+
+            var entities = metadata.GetItems<EntityType>(DataSpace.CSpace).ToList();
+
+            var clrTypes = entities.ToDictionary(
+                e => e,
+                e => oCollection.GetClrType(metadata.GetItem<EntityType>(e.FullName, DataSpace.OSpace)));
+
+            var props = 
+                from e in entities
+                from p in e.NavigationProperties
+                let toType = p.ToEndMember.GetEntityType()
+                select new
+                {
+                    FromType = clrTypes[e],
+                    ToType = clrTypes[toType],
+                    ForeignKey = p.GetDependentProperties().Select(dp => dp.Name).FirstOrDefault(),
+                    NavPropName = p.Name,
+                    p.RelationshipType,
+                };
+
+            // NavigationProperty.RelationshipType is what joins two NavigationProperties
+            var res = props.ToList();
+        }
+    }
+}
