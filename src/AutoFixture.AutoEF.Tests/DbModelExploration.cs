17ce6aa3f1c04fa16eff7a2828a476a5097d11e1
Alex Gill <alexandermichaelfoxgill@hotmail.co.uk>
2015-02-24 12:11:30 +0000

    saving changes

--- src/AutoFixture.AutoEF.Tests/DbModelExploration.cs
+++ src/AutoFixture.AutoEF.Tests/DbModelExploration.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.Core.Metadata.Edm;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
@@ -63,10 +65,13 @@ namespace AutoFixture.AutoEF.Tests
             using (var context = Get<ManyToManyContext>())
                 GetAllForeignKeys(context);
         }
+    }
 
-        public void GetAllForeignKeys(DbContext context)
+    public class DbContextGraphFactory
+    {
+        public IEnumerable<IRelationship> GetRelationships(DbContext context)
         {
-            var metadata = ((IObjectContextAdapter) context).ObjectContext.MetadataWorkspace;
+            var metadata = ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
 
             var oCollection = (ObjectItemCollection)metadata.GetItemCollection(DataSpace.OSpace);
 
@@ -76,21 +81,88 @@ namespace AutoFixture.AutoEF.Tests
                 e => e,
                 e => oCollection.GetClrType(metadata.GetItem<EntityType>(e.FullName, DataSpace.OSpace)));
 
-            var props = 
-                from e in entities
-                from p in e.NavigationProperties
-                let toType = p.ToEndMember.GetEntityType()
-                select new
+            var props =
+                from origin in entities
+                from p in origin.NavigationProperties
+                let dest = p.ToEndMember.GetEntityType()
+                let originMultiplicity = p.RelationshipType.RelationshipEndMembers.First(x => x.GetEntityType() == origin).RelationshipMultiplicity
+                let destMultiplicity = p.RelationshipType.RelationshipEndMembers.First(x => x.GetEntityType() == dest).RelationshipMultiplicity
+                select new NavProperty
                 {
-                    FromType = clrTypes[e],
-                    ToType = clrTypes[toType],
-                    ForeignKey = p.GetDependentProperties().Select(dp => dp.Name).FirstOrDefault(),
-                    NavPropName = p.Name,
-                    p.RelationshipType,
+                    From = clrTypes[origin],
+                    To = clrTypes[dest],
+                    ForeignKeyName = p.GetDependentProperties().Select(dp => dp.Name).FirstOrDefault(),
+                    NavPropertyName = p.Name,
+                    Relationship = p.RelationshipType,
                 };
 
-            // NavigationProperty.RelationshipType is what joins two NavigationProperties
-            var res = props.ToList();
+            return props
+                .GroupBy(p => p.Relationship)
+                .Select(GetRelationship);
         }
+
+        private IRelationship GetRelationship(IEnumerable<NavProperty> properties)
+        {
+            var list = properties.ToList();
+            if (list.Count == 0)
+                throw new ArgumentException("properties is empty");
+            if (list.Select(np => np.Relationship).Distinct().Count() > 1)
+                throw new ArgumentException("properties should share relationship");
+
+            if (list.Count == 1)
+                return new Single(list[0]);
+            if (list.Count == 2)
+                return new Pair(list[0], list[1]);
+
+            throw new InvalidOperationException("more than two properties share a relationship");
+        }
+    }
+
+    public class Relationship
+    {
+        public class Unidirectional : Relationship
+        {
+            public NavProperty X { get; set; }
+
+            public Unidirectional(NavProperty x)
+            {
+                X = x;
+            }
+        }
+    }
+
+    public interface IRelationship
+    {
+    }
+
+    public class Single : IRelationship
+    {
+        public NavProperty X { get; private set; }
+
+        public Single(NavProperty x)
+        {
+            X = x;
+        }
+    }
+
+    public class Pair : IRelationship
+    {
+        public NavProperty A { get; private set; }
+        public NavProperty B { get; private set; }
+
+        public Pair(NavProperty a, NavProperty b)
+        {
+            A = a;
+            B = b;
+        }
+    }
+
+    public class NavProperty
+    {
+        public Type From { get; set; }
+        public Type To { get; set; }
+        public string NavPropertyName { get; set; }
+        public string ForeignKeyName { get; set; }
+        public RelationshipType Relationship { get; set;}
     }
 }
