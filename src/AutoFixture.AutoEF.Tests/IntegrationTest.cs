476aa6f1e10f203f4fbcb03107fe64dad8d7775e
doowruc <doowruc@doowruc.co.uk>
2015-08-03 18:02:21 +0100

    Support IDbSet
    Update DbContextEntityTypesProvider to support collections of IDbSet as
    well as DbSet

--- src/AutoFixture.AutoEF.Tests/IntegrationTest.cs
+++ src/AutoFixture.AutoEF.Tests/IntegrationTest.cs
@@ -110,5 +110,99 @@ namespace AutoFixture.AutoEF.Tests
                 qix.BooId.Should().Be(boo.BooId);
             }
         }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void FixtureCanCreateEFEntityForIDbSet(Foo foo)
+        {
+            foo.Should().NotBeNull();
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void GeneratedPropertyIsNotNullForIDbSet(Foo foo)
+        {
+            foo.Bar.Should().NotBeNull();
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void GeneratedCollectionIsNotEmptyForIDbSet(Bar bar)
+        {
+            bar.Quxes.Should().NotBeEmpty();
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void GeneratedCollectionCanBeClearedForIDbSet(Bar bar)
+        {
+            bar.Quxes.Clear();
+            bar.Quxes.Should().HaveCount(0);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void IdShouldMatchNavigationPropertyIdForIDbSet(Foo foo)
+        {
+            foo.BarId.Should().Be(foo.Bar.Id);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void ParentShouldBeSameObjectForIDbSet(Foo foo)
+        {
+            foo.Bar.Foo.Should().BeSameAs(foo);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void ParentIdShouldBeSameForIDbSet(Foo foo)
+        {
+            foo.Bar.FooId.Should().Be(foo.Id);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void ParentShouldBeSameForCollectionsForIDbSet(Bar bar)
+        {
+            foreach (var qux in bar.Quxes)
+            {
+                (qux.Bar == bar).Should().BeTrue();
+            }
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void RepeatedAccessYieldsSameObjectForIDbSet(Foo foo)
+        {
+            var bar1 = foo.Bar;
+            var bar2 = foo.Bar;
+
+            bar1.Should().BeSameAs(bar2);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void UnfrozenTypeYieldsDifferentObjectForIDbSet(Bar bar, Foo foo)
+        {
+            foo.Bar.Should().NotBeSameAs(bar);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void FrozenTypeYieldsSameObjectForIDbSet([Frozen] Bar bar, Foo foo)
+        {
+            foo.Bar.Should().BeSameAs(bar);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void IdShouldMatchNavigationPropertyIdWhenTableNameIdForIDbSet(Far far)
+        {
+            far.BooId.Should().Be(far.Boo.BooId);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void ParentIdShouldBeSameWhenTableNameIdForIDbSet(Far far)
+        {
+            far.Boo.FarId.Should().Be(far.FarId);
+        }
+
+        [Theory, AutoEFData(typeof(MockDbContextIDbSet))]
+        public void ParentIdShouldBeSameForCollectionsWhenTableNameIdForIDbSet(Boo boo)
+        {
+            foreach (var qix in boo.Qixes)
+            {
+                qix.BooId.Should().Be(boo.BooId);
+            }
+        }
     }
 }
